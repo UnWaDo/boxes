23cdbe72161cb4e1cd15514e368f54244347d704
Leontine Alex <lalex@su-e3.msk.21-school.ru>
2021-06-20 20:34:39 +0300

    URA

--- ft_solution.c
+++ ft_solution.c
@@ -8,29 +8,48 @@ int		ft_try_fill(int **map, int **params, int dim, t_point p);
 int		ft_is_observed_correctly(int **map, int **params, int dim);
 int		ft_is_repeating(int **map, int dim, int x, int y);
 void	ft_print_rect(int **map, int dim);
+int		ft_observe_level(int **map, int dim, int side, int x);
+
+t_point	ft_find_empty_cell_in_row(int **map, int dim, int row)
+{
+	int		i;
+	t_point	p;
+
+	p.x = -1;
+	p.y = -1;
+	i = 0;
+	while (i < dim)
+	{
+		if (map[row][i] == 0)
+		{
+			p.x = row;
+			p.y = i;
+			return (p);
+		}
+		i++;
+	}
+	return (p);
+}
 
 int	ft_find_empty_cell(int **map, int **params, int dim)
 {
 	int		i;
-	int		j;
 	int		end;
 	t_point	p;
 
 	i = 0;
 	while (i < dim)
 	{
-		j = 0;
-		while (j < dim)
+		p = ft_find_empty_cell_in_row(map, dim, i);
+		if (p.x != -1)
 		{
-			if (map[i][j] == 0)
-			{
-				p.x = i;
-				p.y = j;
-				end = ft_try_fill(map, params, dim, p);
-				return (end);
-			}
-			j++;
+			end = ft_try_fill(map, params, dim, p);
+			return (end);
 		}
+		if (ft_observe_level(map, dim, 2, i) != params[2][i])
+			return (0);
+		if (ft_observe_level(map, dim, 3, i) != params[3][i])
+			return (0);
 		i++;
 	}
 	if (!ft_is_observed_correctly(map, params, dim))
